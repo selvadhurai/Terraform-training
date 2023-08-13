555ce1c10e027d1b5c8594c303e0bd83627ac6ad
Selva <47226341+selvadhurai@users.noreply.github.com>
2023-08-12 20:29:24 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,12 @@
 # create VPC on aws
 # Terraform Creating this 
-resource "aws_vpc" "vpc"
-{
+resource "aws_vpc" "vpc" {
   cider_block                         = "172.31.0.0/16'
   instance_tenancy                    = "default"
   enable_dns_hostnames                = true
   assign_generated_ipv6_cidr_block    = true
-tages =    {
-    Name="TerraformVPC-selva"
+tages     = {
+    Name  ="TerraformVPC-selva"
   }
 
 }
