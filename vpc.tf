3c8aa20581d5f1dfe931b6fa4030e9b2ccf342f1
Selva <47226341+selvadhurai@users.noreply.github.com>
2023-08-12 20:26:57 -0400

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+#create VPC on aws
+#Terraform Creating this 
+resource "aws_vpc" "vpc"
+{
+  cider_block                         = "172.31.0.0/16'
+  instance_tenancy                    = "default"
+  enable_dns_hostnames                = true
+  assign_generated_ipv6_cidr_block    = true
+tages =    {
+    Name="TerraformVPC-selva"
+  }
+
+}
